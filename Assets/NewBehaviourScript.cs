f333b5720b2cb6df31cdd448fef1474c79928004
Josimar Santiago <santiago.josimar2011@gmail.com>
2016-12-02 23:35:54 -0400

    first commit maya

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+public class NewBehaviourScript : MonoBehaviour {
+
+	public SocketIOComponent socket;
+	// Use this for init96y7`	6747 k   lization
+	void Start() {
+		Debug.Log("startr: ");
+		StartCoroutine (ConnectedToServer ());
+		socket.On("user connected", OnUserConnected);
+	}
+	IEnumerator ConnectedToServer(){
+		yield return new WaitForSeconds(0.5f);
+		socket.Emit("user login");
+		yield return new WaitForSeconds(1f);
+	}
+	private void OnUserConnected(SocketIOEvent evt){
+		Debug.Log("Get The messagefrom server: " + evt.data);
+	}
+}
